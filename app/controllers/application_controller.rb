893ce6fb145ef4281a301dc7f5119be84eaf1e8c
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-06-26 15:30:07 +0700

    Finish layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "Xin chào nguyễn văn bảy"
-    end
+  before_action :set_locale
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ?
+      locale : I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
