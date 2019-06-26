73cae8bee9814cfa929d994445b9abedc1f62bf0
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-06-26 11:03:04 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  get 'static_pages#home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   root 'application#hello'
 end
