f2184cd73acfaae77ba4004ccd8a30d9d993a750
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-06-27 18:44:01 +0700

    Revert "commit"
    This reverts commit c7d2d90817b3e4a9171b714d03b1dd645b821a37.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home; end
 
-  def help
-  end
+  def help; end
+  
+  def about; end
+
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root 'application#hello'
+
+  scope "(:locale)", locale: /en|vi/ do
+    get "users/new"
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+    get "/contact", to: "static_pages#contact"
+    get "/signup", to: "users#new"
+  end
 end
