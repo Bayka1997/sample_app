db4455adec02fa5e337c6f85e060d8ea768f6d93
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-06-27 09:26:25 +0700

    sua chuong 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   before_action :set_locale
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
@@ -10,5 +9,4 @@ class ApplicationController < ActionController::Base
   def default_url_options
     {locale: I18n.locale}
   end
-    
 end
